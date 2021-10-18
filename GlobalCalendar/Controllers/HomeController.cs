8d37caec69246cb312b86ba0df1a729219d1341e
ravindranathK043 <92525049+ravindranathK043@users.noreply.github.com>
2021-10-18 15:48:16 +0530

    Add files via upload

--- GlobalCalendar/Controllers/HomeController.cs
+++ GlobalCalendar/Controllers/HomeController.cs
@@ -34,23 +34,58 @@ namespace GlobalCalendar.Controllers
             var getcust = dc.UserDetails.ToList();
             return new JsonResult { Data = getcust, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
+        public JsonResult GetCourses()
+        {
+            var getCour = dc.AllCourses.ToList();
+            return new JsonResult { Data = getCour, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
         public JsonResult GetTrainer()
         {
             var getcust = dc.Trainers.ToList();
             return new JsonResult { Data = getcust, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
-        public JsonResult GetTrinings(int Userid)
+        public JsonResult GetTrinings(int Userid,int trainerId) 
         {
             //var events = dc.Courses.ToList();
-            if (Userid == 1)
+            if (Userid != 0)
             {
-                var events = (from obj in dc.Courses
+                var events = (from obj in dc.Courses 
+                              join obj3 in dc.AllCourses on obj.CourseId equals obj3.CourseId
+                              join obj4 in dc.Trainers on obj.TrainerId equals obj4.TrainerId
                               join obj2 in dc.UserDetails on obj.UserId equals obj2.UserId
-                              //where obj.StartDate >=DateTime.Now
+                              where //obj.StartDate >=DateTime.Now &&
+                               obj.CourseId==Userid
                               select new
                               {
                                   Id = obj.Id,
-                                  CourseName = obj.CourseName,
+                                  CourseName = obj3.CourserName,
+                                  TrainerId = obj.TrainerId,
+                                  TrainerName = obj4.TrainerName,
+                                  PreRequisite = obj.PreRequisite,
+                                  Duration = obj.Duration,
+                                  StartDate = obj.StartDate,
+                                  EndDate = obj.EndDate,
+                                  Timings = obj.Timings,
+                                  LinkToJoin = obj.LinkToJoin,
+                                  UserId = obj.UserId,
+                                  UserName = obj2.UserName
+                              }).ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+            else if(trainerId != 0)
+            {
+                var events = (from obj in dc.Courses
+                              join obj3 in dc.AllCourses on obj.CourseId equals obj3.CourseId
+                              join obj4 in dc.Trainers on obj.TrainerId equals obj4.TrainerId
+                              join obj2 in dc.UserDetails on obj.UserId equals obj2.UserId
+                              where //obj.StartDate >=DateTime.Now &&
+                              obj.TrainerId == trainerId
+                              select new
+                              {
+                                  Id = obj.Id,
+                                  CourseName = obj3.CourserName,
+                                  TrainerId = obj.TrainerId,
+                                  TrainerName = obj4.TrainerName,
                                   PreRequisite = obj.PreRequisite,
                                   Duration = obj.Duration,
                                   StartDate = obj.StartDate,
@@ -65,13 +100,16 @@ namespace GlobalCalendar.Controllers
             else
             {
                 var events = (from obj in dc.Courses
+                              join obj3 in dc.AllCourses on obj.CourseId equals obj3.CourseId
+                              join obj4 in dc.Trainers on obj.TrainerId equals obj4.TrainerId
                               join obj2 in dc.UserDetails on obj.UserId equals obj2.UserId
-                              where //obj.StartDate >=DateTime.Now &&
-                              obj.UserId == Userid
+                              //where obj.StartDate >=DateTime.Now
                               select new
                               {
                                   Id = obj.Id,
-                                  CourseName = obj.CourseName,
+                                  CourseName = obj3.CourserName,
+                                  TrainerId = obj.TrainerId,
+                                  TrainerName = obj4.TrainerName,
                                   PreRequisite = obj.PreRequisite,
                                   Duration = obj.Duration,
                                   StartDate = obj.StartDate,
@@ -83,7 +121,6 @@ namespace GlobalCalendar.Controllers
                               }).ToList();
                 return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
             }
-
         }
 
 
@@ -93,36 +130,36 @@ namespace GlobalCalendar.Controllers
             var status = false;
 
 
-            var UserExist = dc.UserDetails.Where(x => x.UserId == e.UserId).FirstOrDefault();
-            if (UserExist != null)
-            {
+            //var UserExist = dc.UserDetails.Where(x => x.UserId == e.UserId).FirstOrDefault();
+            //if (UserExist != null)
+            //{
 
-            }
-            else
-            {
-                var UserExist2 = (from obj in dc.UserDetails
-                                  where obj.UserName == Cust
-                                  select obj).ToList();
-                if (UserExist2.Count > 0)
-                {
-                    e.UserId = UserExist2[0].UserId;
-                }
-                else
-                {
-                    UserDetail users = new UserDetail();
-                    users.UserName = Cust;
-                    users.Password = Cust;
-                    dc.UserDetails.Add(users);
-                    dc.SaveChanges();
-                    e.UserId = users.UserId;
-                }
-            }
+            //}
+            //else
+            //{
+            //    var UserExist2 = (from obj in dc.UserDetails
+            //                      where obj.UserName == Cust
+            //                      select obj).ToList();
+            //    if (UserExist2.Count > 0)
+            //    {
+            //        e.UserId = UserExist2[0].UserId;
+            //    }
+            //    else
+            //    {
+            //        UserDetail users = new UserDetail();
+            //        users.UserName = Cust;
+            //        users.Password = Cust;
+            //        dc.UserDetails.Add(users);
+            //        dc.SaveChanges();
+            //        e.UserId = users.UserId;
+            //    }
+            //}
             if (e.Id > 0)
             {
                 var getdata = dc.Courses.Where(x => x.Id == e.Id).FirstOrDefault();
                 if (getdata != null)
                 {
-                    getdata.CourseName = e.CourseName;
+                    getdata.CourseId = e.CourseId;
                     getdata.PreRequisite = e.PreRequisite;
                     getdata.StartDate = e.StartDate;
                     getdata.EndDate = e.EndDate;
@@ -130,6 +167,7 @@ namespace GlobalCalendar.Controllers
                     getdata.Duration = e.Duration;
                     getdata.LinkToJoin = e.LinkToJoin;
                     getdata.UserId = e.UserId;
+                    getdata.TrainerId = e.TrainerId;
                 }
             }
             else
